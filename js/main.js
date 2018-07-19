1f531b3fa547e6b4d4afd0f5ad2ed01405a5fc6b
Khaled Sarhan <khaled_sarhan2003@yahoo.com>
2018-07-19 18:40:02 +0300

    Add an alternate text with a description to images

--- js/main.js
+++ js/main.js
@@ -161,6 +161,8 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt', 'Photograph of ' + restaurant.name + ' restaurant');
+  image.setAttribute('title', restaurant.name);
   li.append(image);
 
   const name = document.createElement('h1');
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,6 +89,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt', 'Photograph of ' + restaurant.name + ' restaurant');
+  image.setAttribute('title', restaurant.name);
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
